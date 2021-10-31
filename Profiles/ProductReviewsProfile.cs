913b2ae04ccdc670ea2e54d63abe7c130ba0b369
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-31 12:20:35 +0000

    Created the ProductReviewsProfile for AutoMapper to map entities.

---
+++ Profiles/ProductReviewsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.Profiles
+{
+    public class ProductReviewsProfile : Profile
+    {
+        public ProductReviewsProfile()
+        {
+            //CreateMap
+        }
+    }
+}

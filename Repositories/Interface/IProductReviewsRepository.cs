0d282236bb633efc79a8af9d07ad4336bcef8556
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-31 10:23:21 +0000

    Implemented the IProductReviewsRepository.

---
+++ Repositories/Interface/IProductReviewsRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductReviews.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.Repositories.Interface
+{
+    public interface IProductReviewsRepository
+    {
+        public Task<IEnumerable<ProductReviewModel>> GetAllProductReviewsAsync();
+        public Task<ProductReviewModel> GetProductReviewAsync(int ID);
+        public Task<ProductReviewModel> CreateProductReviewAsync(ProductReviewModel productReviewModel);
+        public Task<ProductReviewModel> UpdateProductReview(ProductReviewModel productReviewModel);
+        public Task SaveChangesAsync();
+    }
+}

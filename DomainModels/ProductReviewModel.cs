3a6d4529075699048cf6a664473487dda362b869
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-31 10:15:18 +0000

    Implemented the intial ProductReview Domain Model.

---
+++ DomainModels/ProductReviewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.DomainModels
+{
+    public class ProductReviewModel
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        [Required]
+        public string ReviewHeader { get; set; }
+        [Required]
+        public string ReviewContent { get; set; }
+        [Required]
+        public DateTime ReviewDate { get; set; }
+        [Required]
+        public int ProductID { get; set; }
+        [Required]
+        public bool ReviewIsHidden { get; set; }
+    }
+}

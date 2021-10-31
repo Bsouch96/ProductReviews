5f763e348a4577758c773d513cd58f939c5c0b0a
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-31 12:44:08 +0000

    Implemented the GetAll and GetIndividual API functions.

---
+++ Controllers/ProductReviews.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductReviews.DTOs;
+using ProductReviews.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.Controllers
+{
+    [Route("api/ProductReviews")]
+    [ApiController]
+    public class ProductReviews : ControllerBase
+    {
+        public IProductReviewsRepository _productReviewsRepository;
+        private readonly IMapper _mapper;
+
+        public ProductReviews(IProductReviewsRepository productReviewsRepository,
+            IMapper mapper)
+        {
+            _productReviewsRepository = productReviewsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductReviewReadDTO>>> GetAllProductReviews()
+        {
+            var productReviews = await _productReviewsRepository.GetAllProductReviewsAsync();
+
+            return Ok(_mapper.Map<IEnumerable<ProductReviewReadDTO>>(productReviews));
+        }
+
+        [HttpGet("{ID}")]
+        public async Task<ActionResult<ProductReviewReadDTO>> GetProductReview(int ID)
+        {
+            var productReview = await _productReviewsRepository.GetProductReviewAsync(ID);
+
+            if (productReview != null)
+                return Ok(_mapper.Map<ProductReviewReadDTO>(productReview));
+
+            return NotFound();
+        }
+    }
+}

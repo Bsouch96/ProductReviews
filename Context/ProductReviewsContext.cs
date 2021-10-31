7aff721a1c0f4aa0d02e02cbc3ebe46c222f9a32
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-31 11:11:23 +0000

    Scaffolded the SqlProductReviews concrete class and implemented the ProductReviewsContext class.

---
+++ Context/ProductReviewsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductReviews.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.Context
+{
+    public class ProductReviewsContext : DbContext
+    {
+        private DbSet<ProductReviewModel> _productReviewsContext { get; set; }
+        public ProductReviewsContext(DbContextOptions<ProductReviewsContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Repositories/Concrete/SqlProductReviewsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviews.Repositories.Concrete
+{
+    public class SqlProductReviewsRepository
+    {
+
+        public SqlProductReviewsRepository()
+        {
+
+        }
+    }
+}
